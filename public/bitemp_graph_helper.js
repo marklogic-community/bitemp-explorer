9526611280a99b0928ab8c58e9ae40c75fdc1cde
lrhruska <lrhruska@calpoly.edu>
2015-08-25 12:54:45 -0700

    rebasing

--- public/bitemp_graph_helper.js
+++ public/bitemp_graph_helper.js
@@ -169,6 +169,20 @@ function save(chart) {
   var data = document.getElementById('contents').value.replace(/\n/g, '');
   data = data.replace(/\//g, '');
   data = jQuery.parseJSON(data);
+  var uri = chart.getCurrentURI();
+  var logURI = chart.getLogicalURI();
+  console.log('Logical uri: ' + logURI);
+  console.log('URI: '+uri);
+  
+  var collArr = getDocColls(uri);
+  var tempCollections = getTemporalColl(uri);
+  var tempCollArr = tempCollections['temporal-collection-default-list']['list-items']['list-item'];
+
+  var tempColl;
+  if (collArr && tempCollArr) {
+    collArr = collArr.collections;
+    tempColl = findCommonColl(collArr, tempCollArr);
+  }
 
   var uri = chart.getCurrentURI();
   var logURI = chart.getLogicalURI();
@@ -198,12 +212,17 @@ function save(chart) {
     cancel(chart);
     loadData(logURI);
   };
+<<<<<<< HEAD
   var fail = function(response) {
     console.log('PUT didn\'t work');
     if (response['responseJSON']['errorResponse']['messageCode'] === 'TEMPORAL-SYSTEMTIME-BACKWARDS') {
       window.alert('Temporal time cannot go backwards, please use a future time');
       //cancel(chart);
     }
+=======
+  var fail = function(data) {
+    window.alert('POST didn\'t work: ' + data);
+>>>>>>> rebasing
   };
   var contType;
   if (uri.endsWith('.json')) {
@@ -214,16 +233,29 @@ function save(chart) {
     //data = jQuery.stringify
   }
 
+<<<<<<< HEAD
   $.ajax({
     type: 'PUT',
     format: contType,
     processData: false,
     url: url,
+=======
+  console.log('data is ' + data + 'and contType is ' + contType);
+  
+  $.ajax({
+    type: 'POST',
+    format: contType,
+    url: '/v1/documents?uri='+uri,
+>>>>>>> rebasing
     data: data,
     success: success,
-    error: fail
+    error: fail 
   });
+<<<<<<< HEAD
 
+=======
+  
+>>>>>>> rebasing
 }
 
 
@@ -262,16 +294,38 @@ function saveNewDoc(chart) {
   var format = formatList.options[formatList.selectedIndex].value;
 
   if (format === 'JSON') {
+<<<<<<< HEAD
     data = JSON.stringify(data);
+=======
+<<<<<<< HEAD
+=======
+    docData = jQuery.parseJSON(data);
+>>>>>>> rebasing
+>>>>>>> rebasing
   } else {
     data = data.replace(/ /g, '');
     docData = jQuery.parseXML(data);
   }
 
   $.ajax({
+<<<<<<< HEAD
     url: '/v1/documents/?uri='+newURI+'&temporal-collection='+selectedColl,
     type: 'PUT',
     data: data,
+=======
+<<<<<<< HEAD
+    url: '/v1/documents/?temporal-collection=' + selectedColl,
+    uri: newURI,
+    type: 'PUT',
+    data: data,
+    processData: false,
+=======
+    url: '/v1/documents?uri=' + newURI,
+    uri: newURI,
+    type: 'PUT',
+    data: JSON.stringify(docData),
+>>>>>>> rebasing
+>>>>>>> rebasing
     success: function(data) {
       loadData(newURI);
     },
