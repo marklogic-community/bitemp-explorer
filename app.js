63b9b9d84d5796a04c9654b28628cfcb5a9783a2
Hilary Schulz <hils124@yahoo.com>
2015-08-31 10:59:29 -0700

    Changed webpage names

--- app.js
+++ app.js
@@ -90,11 +90,11 @@ app.get('/delete', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/search.html');
 });
 
-app.get('/search', function(req, res) {
-  res.sendfile(__dirname + '/search.html');
+app.get('/view', function(req, res) {
+  res.sendfile(__dirname + '/view.html');
 });
 
 /* start listening */
